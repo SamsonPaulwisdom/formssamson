dcc5ccc5c0aaa69c5f7a565ceed8b6dc016db7b0
Paul Wisdom <itmm103samsonpaulwisdom@gmail.com>
2018-06-27 23:39:45 +0800

    Send file to server

---
+++ welcome.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<p>
+	<h1>Your Data !</h1>
+
+	<br>
+	
+Welcome <?php echo $_POST["name"]; ?><br>
+Your Name is: <?php echo $_POST["name"]; ?>
+<br>
+Date of Birth is: <?php echo $_POST["dateofbirth"] ?>
+<br>
+Address : <?php echo $_POST["address"]; ?>
+<br>
+
+Email : <?php echo $_POST["email"]; ?>
+<br>
+
+</p>
+
+</p>
+</body>
+</html>
